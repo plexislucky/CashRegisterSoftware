b6c23ff558b0d43b6c8b128cf4fd7e81ac6b7553
plex <plexislucky@gmail.com>
2024-06-01 12:23:09 +0300

    Added working number field
    Added a text field to show user input from the custom buttons.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-
+NUMBER_FIELD_CONTENT: str = ""
 # ------ CUSTOM WIDGETS START ------
 
 class CustomButton:
@@ -37,6 +37,13 @@ def ClickHandler(event): # This method only gets called on LEFT click
         if event.x >= button.x and event.y >= button.y and event.x <= button.x+50 and event.y <= button.y+50:
             button.OnMouseRelease()
 
+def AddToInputField(value: str):
+    global NUMBER_FIELD_CONTENT
+    NUMBER_FIELD_CONTENT += value
+    canvas.itemconfig(numberFieldText, text=NUMBER_FIELD_CONTENT)
+
+
+
 
 # ------ TKINTER BOILERPLATE BULLSHIT BELOW ------
 
@@ -48,19 +55,21 @@ root.resizable(False, False) # please excuse my dope ass swag
 canvas = Canvas(root, width=500, height=500)
 canvas.place(x=0, y=0)
 
+numberFieldText = canvas.create_text(490, 0, text=NUMBER_FIELD_CONTENT, font=("Helvetica 22"), anchor=NE)
+
 # C++ developers fear my long ass manually-filled lists...
 buttons: list[CustomButton] = [
-    CustomButton(0, 0, text="7", command=lambda: print(7)),
-    CustomButton(51, 0, text="8", command=lambda: print(8)),
-    CustomButton(102, 0, text="9", command=lambda: print(9)),
-    CustomButton(0, 51, text="4"),
-    CustomButton(51, 51, text="5"),
-    CustomButton(102, 51, text="6"),
-    CustomButton(0, 102, text="1"),
-    CustomButton(51, 102, text="2"),
-    CustomButton(102, 102, text="3"),
-    CustomButton(0, 153, text="0"),
-    CustomButton(51, 153, text="."),
+    CustomButton(0, 0, text="7", command=lambda: AddToInputField("7")),
+    CustomButton(51, 0, text="8", command=lambda: AddToInputField("8")),
+    CustomButton(102, 0, text="9", command=lambda: AddToInputField("9")),
+    CustomButton(0, 51, text="4", command=lambda: AddToInputField("4")),
+    CustomButton(51, 51, text="5", command=lambda: AddToInputField("5")),
+    CustomButton(102, 51, text="6", command=lambda: AddToInputField("6")),
+    CustomButton(0, 102, text="1", command=lambda: AddToInputField("1")),
+    CustomButton(51, 102, text="2", command=lambda: AddToInputField("2")),
+    CustomButton(102, 102, text="3", command=lambda: AddToInputField("3")),
+    CustomButton(0, 153, text="0", command=lambda: AddToInputField("0")),
+    CustomButton(51, 153, text=".", command=lambda: AddToInputField(".")),
     CustomButton(102, 153, width=151, text="Cash payment", fill="lime"),
     CustomButton(153, 102, width=100, text="Total"),
     CustomButton(153, 51, width=100, text="Cancel last", fill="red"),
